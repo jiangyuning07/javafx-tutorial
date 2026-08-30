2c774950d7a04aa5960e9bd94703a41a742fc815
Jiang Yuning <137623527+jiangyuning07@users.noreply.github.com>
2026-08-30 21:49:57 +0800

    Differentiate user and Duke dialog boxes

--- src/main/java/DialogBox.java
+++ src/main/java/DialogBox.java
@@ -3,6 +3,9 @@ import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.scene.Node;
 
 public class DialogBox extends HBox {
 
@@ -19,5 +22,28 @@ public class DialogBox extends HBox {
         this.setAlignment(Pos.TOP_RIGHT);
 
         this.getChildren().addAll(text, displayPicture);
+    }
+
+    /**
+     * Flips the dialog box so that the image is on the left
+     * and the text is on the right.
+     */
+    private void flip() {
+        this.setAlignment(Pos.TOP_LEFT);
+
+        ObservableList<Node> children =
+                FXCollections.observableArrayList(this.getChildren());
+        FXCollections.reverse(children);
+        this.getChildren().setAll(children);
+    }
+
+    public static DialogBox getUserDialog(String text, Image image) {
+        return new DialogBox(text, image);
+    }
+
+    public static DialogBox getDukeDialog(String text, Image image) {
+        DialogBox dialogBox = new DialogBox(text, image);
+        dialogBox.flip();
+        return dialogBox;
     }
 }
