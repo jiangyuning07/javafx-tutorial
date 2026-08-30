3fdfe269bf2558543912de173b779ef77213fa63
Jiang Yuning <137623527+jiangyuning07@users.noreply.github.com>
2026-08-30 21:48:42 +0800

    Add Duke responses

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -21,6 +21,7 @@ public class Main extends Application {
             this.getClass().getResourceAsStream("/images/DaUser.png"));
     private Image dukeImage = new Image(
             this.getClass().getResourceAsStream("/images/DaDuke.png"));
+    private Duke duke = new Duke();
 
     @Override
     public void start(Stage stage) {
@@ -83,7 +84,13 @@ public class Main extends Application {
      * the dialog container. Clears the user input after processing.
      */
     private void handleUserInput() {
-        dialogContainer.getChildren().addAll(new DialogBox(userInput.getText(), userImage));
+        String userText = userInput.getText();
+        String dukeText = duke.getResponse(userText);
+
+        dialogContainer.getChildren().addAll(
+                new DialogBox(userText, userImage),
+                new DialogBox(dukeText, dukeImage));
+
         userInput.clear();
     }
 }
